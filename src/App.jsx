9eac0e4111f38ba343d3ecea2c1e052944619bb5
Himanshu <himanshukakran8@gmail.com>
2025-07-25 14:32:53 +0530

    working on home ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import AppRoutes from './AppRoutes';
 
 
 const App = () => {
   return (
     <>
       <Navbar />
+    <AppRoutes/>
    
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import AppRoutes from "./AppRoutes";
+// import AppRoutes from "./AppRoutes";
 
 const Home = () => {
 
-
-
-    
-      
  return (
-    <Router>
-      <Navbar />
-      <AppRoutes />
-    </Router>
+    <div className="flex flex-col justify-center items-center p-8 bg-slate-900 text-white h-screen ">
+      <h2 className="text-7xl font-bold ">
+        Welcome to Github Profile View
+      </h2>
+      <h4 className="text-2xl">
+        Now, you can find anyone with their GitHub username
+      </h4>
+    </div>
   );
 }
 
